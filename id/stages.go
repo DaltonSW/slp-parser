8171fbb30af674b56d73a3dfeba0ef00de3684f4
Dalton Williams <daltonstwilliams@gmail.com>
2025-04-29 15:54:37 -0500

    Migrate enums to id in different files

---
+++ id/stages.go
@@ -0,0 +1,32 @@
+package id
+
+// Internal Stage IDs
+const (
+	PrincessPeachsCastleStage = 0x02
+	RainbowCruiseStage        = 0x03
+	KongoJungleStage          = 0x04
+	JungleJapesStage          = 0x05
+	GreatBayStage             = 0x06
+	HyruleTempleStage         = 0x07
+	BrinstarStage             = 0x08
+	BrinstarDepthsStage       = 0x09
+	YoshisStoryStage          = 0x0A
+	YoshisIslandStage         = 0x0B
+	FountainofDreamsStage     = 0x0C
+	GreenGreensStage          = 0x0D
+	CorneriaStage             = 0x0E
+	VenomStage                = 0x0F
+	PokemonStadiumStage       = 0x10
+	PokeFloatsStage           = 0x11
+	MuteCityStage             = 0x12
+	BigBlueStage              = 0x13
+	OnettStage                = 0x14
+	FoursideStage             = 0x15
+	IcicleMountainStage       = 0x16
+	MushroomKingdomStage      = 0x18
+	MushroomKingdomIIStage    = 0x19
+	FlatZoneStage             = 0x1B
+	DreamLandStage            = 0x1C
+	YoshisIsland64Stage       = 0x1D
+	KongoJungle64Stage        = 0x1E
+)
---
+++ id/states.go
@@ -0,0 +1,3 @@
+package id
+
+type ActionState uint16
